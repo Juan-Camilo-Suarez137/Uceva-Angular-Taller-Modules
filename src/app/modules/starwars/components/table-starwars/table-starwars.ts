b02d590a269e80ea51c4137d9c69f0d11a99deb5
Camilo Suarez <juan.suarez04@uceva.edu.co>
2026-09-02 15:37:14 -0500

    feat: implementar renderizado de tabla de personajes

--- src/app/modules/starwars/components/table-starwars/table-starwars.ts
+++ src/app/modules/starwars/components/table-starwars/table-starwars.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { StarWarsCharacter } from '../../interfaces/starwars';
 
 @Component({
   selector: 'app-table-starwars',
   standalone: false,
   templateUrl: './table-starwars.html',
-  styleUrl: './table-starwars.scss',
+  styleUrl: './table-starwars.scss'
 })
 export class TableStarwars {
-
+  @Input() characters: StarWarsCharacter[] = [];
 }
--- src/app/modules/starwars/services/starwars.ts
+++ src/app/modules/starwars/services/starwars.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { StarWarsCharacter } from '../interfaces/starwars';
+import { STARWARS_CONFIG } from '../../../core/config/starwars.config';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class Starwars {
-  
+
+  /**
+   * Obtiene el listado de personajes de Star Wars
+   * desde la configuración definida en el Core.
+   */
+  getCharacters(): Observable<StarWarsCharacter[]> {
+    return of(STARWARS_CONFIG);
+  }
 }
