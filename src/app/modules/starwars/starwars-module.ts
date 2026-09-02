f4cb0fc1e0c9ac7e9c03f2f01833f50619e5f07d
Camilo Suarez <juan.suarez04@uceva.edu.co>
2026-09-02 14:49:57 -0500

    feat/ se genero estructura base del modulo con enrutamiento

---
+++ src/app/modules/starwars/starwars-module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { StarwarsRoutingModule } from './starwars-routing-module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    StarwarsRoutingModule
+  ]
+})
+export class StarwarsModule { }
