0606718353fd0d9731549fac8d7f7da9a094ae7b
Sebastian <sebastian.morales01@uceva.edu.co>
2026-09-04 10:06:17 -0500

    feat(nasa): generar modulo nasa con routing

---
+++ src/app/modules/nasa/nasa-module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NasaRoutingModule } from './nasa-routing-module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NasaRoutingModule
+  ]
+})
+export class NasaModule { }
