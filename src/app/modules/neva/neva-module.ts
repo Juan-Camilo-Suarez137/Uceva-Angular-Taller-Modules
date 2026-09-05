3e674c8ba4849b25c9d55f5059cea4a6c06ef643
Moon345x <juanestevantiago@gmail.com>
2026-09-04 21:07:53 -0500

    feat: scaffolding del módulo neva con routing

---
+++ src/app/modules/neva/neva-module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NevaRoutingModule } from './neva-routing-module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NevaRoutingModule
+  ]
+})
+export class NevaModule { }
