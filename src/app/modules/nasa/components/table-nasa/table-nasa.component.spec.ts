2b76cc69f0bb56639a6ec4c1e17b5547dc5b6b7b
Sebastian <sebastian.morales01@uceva.edu.co>
2026-09-04 21:33:07 -0500

    test(nasa): agrega pruebas del servicio HttpClient y componentes

--- src/app/modules/nasa/components/table-nasa/table-nasa.component.spec.ts
+++ src/app/modules/nasa/components/table-nasa/table-nasa.component.spec.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TableNasaComponent } from './table-nasa.component';
@@ -8,7 +9,8 @@ describe('TableNasaComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [TableNasaComponent]
+      declarations: [TableNasaComponent],
+      imports: [CommonModule],
     })
     .compileComponents();
 
--- src/app/modules/nasa/pages/list-nasa/list-nasa.component.spec.ts
+++ src/app/modules/nasa/pages/list-nasa/list-nasa.component.spec.ts
@@ -1,14 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of, throwError } from 'rxjs';
 
+import { TableNasaComponent } from '../../components/table-nasa/table-nasa.component';
+import { NasaService } from '../../services/nasa.service';
 import { ListNasaComponent } from './list-nasa.component';
 
 describe('ListNasaComponent', () => {
   let component: ListNasaComponent;
   let fixture: ComponentFixture<ListNasaComponent>;
+  let nasaServiceMock: { getAllLaunches: jest.Mock };
 
   beforeEach(async () => {
+    nasaServiceMock = {
+      getAllLaunches: jest.fn(() => of({ count: 0, next: '', previous: null, results: [] })),
+    };
+
     await TestBed.configureTestingModule({
-      declarations: [ListNasaComponent]
+      declarations: [ListNasaComponent, TableNasaComponent],
+      imports: [CommonModule],
+      providers: [
+        {
+          provide: NasaService,
+          useValue: nasaServiceMock,
+        },
+      ],
     })
     .compileComponents();
 
@@ -19,5 +35,17 @@ describe('ListNasaComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should log service errors', () => {
+    const error = new Error('Error loading launches');
+    nasaServiceMock.getAllLaunches.mockReturnValueOnce(throwError(() => error));
+    const consoleError = jest.spyOn(console, 'error').mockImplementation();
+
+    const errorFixture = TestBed.createComponent(ListNasaComponent);
+    errorFixture.detectChanges();
+
+    expect(consoleError).toHaveBeenCalledWith(error);
+    consoleError.mockRestore();
   });
 });
