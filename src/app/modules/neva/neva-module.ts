d75dd9b61161ecc042cef60b15ab724c531e1ef4
Moon345x <juanestevantiago@gmail.com>
2026-09-04 21:10:44 -0500

    feat: componente de tabla del módulo neva

--- src/app/modules/neva/neva-module.ts
+++ src/app/modules/neva/neva-module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NevaRoutingModule } from './neva-routing-module';
+import { TableNeva } from './components/table-neva/table-neva';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TableNeva
+  ],
   imports: [
     CommonModule,
     NevaRoutingModule
